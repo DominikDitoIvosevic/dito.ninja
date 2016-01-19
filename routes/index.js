ff70ff08b9a1f3c5f5c3bcd3966ff00aa91b8910
Dominik Ivošević <dominik.dito@gmail.com>
2016-01-19 13:22:04 +0100

    Test commit for Unity WWW

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/postscore', function(req, res, next) {
+    console.log("posting");
+    console.log(req);
+    if (next) next();
+});
+
+router.get('/postscore', function(req, res, next) {
+    console.log("getting");
+    console.log(req);
+    if (next) next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "Dito's laboratory" });
 });
+
 
 module.exports = router;
